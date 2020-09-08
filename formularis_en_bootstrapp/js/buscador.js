e3d75af876c7cfc674be90dab6d7f14f1a5959cc
pedro79 <pecepla79@gmail.com>
2020-09-08 09:45:53 +0200

    ejercicio validacio nde formularios acabado

--- formularis_en_bootstrapp/js/buscador.js
+++ formularis_en_bootstrapp/js/buscador.js
@@ -1,6 +1,8 @@
-const form = document.getElementById('formSearch');
+const forms = document.getElementById('formSearch');
 
 function validarSearch(){
+	var acumErrores = 0;
+
     var modalSearch = document.getElementById('search');  
 
     if(modalSearch.value == "") {
@@ -12,12 +14,16 @@ function validarSearch(){
 		document.getElementById("errorSearch").textContent = "el campo deber tener ms de 3 caracteres";
 	
 	}
-	
+	if (acumErrores > 0){
+        return false;
+    }else{
+		return true;
+	}
 
 }
-form.addEventListener('blur', (event) => {
-    console.log(event);
-    event.preventDefault;
+forms.addEventListener('blur', (event) => {
+   
+    event.preventDefault();
 	if(event.target.value!='') event.target.classList.remove('is-invalid');
 
 }, true);
